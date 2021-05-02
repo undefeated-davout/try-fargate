333ffb5ca681b71558d3a07aa1948abfe520dfcb
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-05-02 12:52:10 +0900

    add: echoサーバからECS RunTask実行する処理追加

--- web-server/go.mod
+++ web-server/go.mod
@@ -3,12 +3,14 @@ module try-fargate/web-server
 go 1.16
 
 require (
+	github.com/aws/aws-sdk-go v1.38.30
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/net v0.0.0-20210501222612-f8dd838d8c7d // indirect
+	golang.org/x/net v0.0.0-20210502030024-e5908800b52b // indirect
 	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887 // indirect
 )
--- web-server/main.go
+++ web-server/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecs"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -11,12 +18,19 @@ func main() {
 	// Echo instance
 	e := echo.New()
 
+	// .env読み込み
+	err := godotenv.Load(fmt.Sprintf("./env/%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	// Routes
 	e.GET("/", hello)
+	e.GET("/hello/task", runHelloTask)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
@@ -27,5 +41,47 @@ func hello(c echo.Context) error {
 	return c.JSON(http.StatusOK,
 		map[string]string{
 			"success": "echo server is working!!",
+		})
+}
+
+func runHelloTask(c echo.Context) error {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("ECS_REGION"))},
+	)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	svc := ecs.New(sess)
+	input := &ecs.RunTaskInput{
+		Cluster:        aws.String(os.Getenv("ECS_CLUSTER")),         // クラスター名
+		TaskDefinition: aws.String(os.Getenv("ECS_TASK_DEFINITION")), // バージョン指定しない場合はLATESTが選択される
+	}
+
+	input.NetworkConfiguration = &ecs.NetworkConfiguration{
+		AwsvpcConfiguration: &ecs.AwsVpcConfiguration{
+			Subnets: aws.StringSlice([]string{ // サブネットID
+				os.Getenv("ECS_SUBNET_1"),
+				os.Getenv("ECS_SUBNET_2"),
+			}),
+			AssignPublicIp: aws.String("ENABLED"), // 必要に応じて
+		},
+	}
+
+	input.LaunchType = aws.String("FARGATE")
+
+	result, err := svc.RunTask(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			fmt.Println(aerr.Error())
+		}
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println(result)
+	return c.JSON(http.StatusOK,
+		map[string]string{
+			"success": "runHelloTask is success",
 		})
 }
